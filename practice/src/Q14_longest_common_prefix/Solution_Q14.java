f9a2771e90e1e0f288ca629582f641d5cb2c64af
mozzi327 <mozzi327@gmail.com>
2023-06-05 16:16:38 +0900

    Q14_longest_common_prefix

---
+++ practice/src/Q14_longest_common_prefix/Solution_Q14.java
@@ -0,0 +1,25 @@
+package Q14_longest_common_prefix;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution_Q14 {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.length() - s2.length();
+            }
+        });
+
+        String first = strs[0];
+        for(int i = 0; i < first.length(); i++) {
+            char c = first.charAt(i);
+            for(int j = 1; j < strs.length; j++) {
+                if(strs[j].charAt(i) != c) return first.substring(0, i);
+            }
+        }
+
+        return first;
+    }
+}
