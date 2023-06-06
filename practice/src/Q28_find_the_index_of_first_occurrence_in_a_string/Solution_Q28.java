0f664c0b94c1a7a257fae23f342f97f209c4aa9d
mozzi327 <mozzi327@gmail.com>
2023-06-06 09:56:40 +0900

    Q28_find_the_index_of_first_occurrence_in_a_string

---
+++ practice/src/Q28_find_the_index_of_first_occurrence_in_a_string/Solution_Q28.java
@@ -0,0 +1,7 @@
+package Q28_find_the_index_of_first_occurrence_in_a_string;
+
+public class Solution_Q28 {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
