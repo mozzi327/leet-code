d397d57fd36b3f2075d63dd7848f6af4f13ba75e
mozzi327 <mozzi327@gmail.com>
2023-06-06 09:41:51 +0900

    Q26_remove_duplicates_from_sorted_array

---
+++ practice/src/Q26_remove_duplicates_from_sorted_array/Solution_Q26.java
@@ -0,0 +1,13 @@
+package Q26_remove_duplicates_from_sorted_array;
+
+public class Solution_Q26 {
+    public int removeDuplicates(int[] nums) {
+        int idx = 1;
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] != nums[i + 1]) {
+                nums[idx++] = nums[i + 1];
+            }
+        }
+        return idx;
+    }
+}
