36cea26f2aa85f8aec7a282598e38c71fae945b0
mozzi327 <mozzi327@gmail.com>
2023-06-06 10:05:56 +0900

    Q35_search_insert_position

---
+++ practice/src/Q35_search_insert_position/Solution_Q35.java
@@ -0,0 +1,13 @@
+package Q35_search_insert_position;
+
+public class Solution_Q35 {
+    public int searchInsert(int[] nums, int target) {
+        int idx = 0;
+        while(idx < nums.length) {
+            if(nums[idx] == target) return idx;
+            else if(nums[idx] < target) idx++;
+            else break;
+        }
+        return idx;
+    }
+}
